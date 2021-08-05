dc92ffc928a68d62474cd7358b134e87f6956ab8
SantoshNGitHub <sksantosh1993@gmail.com>
2021-08-05 21:08:42 +0530

    undo push

--- Sample.java
+++ Sample.java
@@ -1,18 +1,17 @@
 /* This is a simple Java program. 
    FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
+class HelloWorld {
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-	System.out.println("Hello World Complete");
-	System.out.println("Hello World Complete2");
-	System.out.println("Hello World Complete3");
-	System.out.println("Hello World Complete4");
-	System.out.println("Hello World Complete5");
-	System.out.println("Hello World Complete7");
- 
-    } 
+    public static void main(String args[]) {
+        System.out.println("Hello, World");
+        System.out.println("Hello World Complete");
+        System.out.println("Hello World Complete2");
+        System.out.println("Hello World Complete3");
+        System.out.println("Hello World Complete4");
+        System.out.println("Hello World Complete5");
+        System.out.println("Hello World Complete7");
+
+        System.out.println("Undo the push of this line");
+    }
 } 
